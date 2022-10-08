99f86d6c58eb39078df0523a3ed968c7117549fd
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-10-08 17:47:56 -0400

    worked on JS and updated some html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+//Variables
+var timeBlock = $("textarea[id^='hour']");
+var saveBtn = $(".saveBtn");
+var toDo;
+var newToDo = [];
+
 //Jumbotron Header
 function currentDate() {
     currentDate = moment().format("MMMM Do YYYY, h a");
@@ -13,9 +19,59 @@ function saveButton(event) {
     toDo = $(this).siblings("textarea[id^='hour']").val();
 
     storeTodo();
+
+    //keeps information on table (NOT WORKING)
+    function getToDo() {
+        localStorage.getItem(toDo);
+        console.log(toDo);
+
+        $(this).siblings("textarea[id^='hour']").text(toDo);
+    }
+
+    getToDo();
+
 };
 
 // Stores the hour and to do to Local Storage
 function storeTodo() {
     localStorage.setItem(hour + " 'o Clock", "To do: " + toDo);
 };
+
+
+
+// Compares times to change colors for present(orange), future(green) or past(grey)
+function blockColors() {
+    timeBlock.each(function () {
+        var colorTime = $(this).attr("id").split("-")[1];
+        colorTime = parseInt(moment(colorTime, "H").format("H"));
+        currentTime = parseInt(moment().format("H"));
+
+        if (currentTime < colorTime) {
+            $(this).removeClass("past present");
+            $(this).addClass("future");
+        } else if (currentTime === colorTime) {
+            $(this).removeClass("past future");
+            $(this).addClass("present");
+        } else if (currentTime > colorTime) {
+            $(this).removeClass("present future");
+            $(this).addClass("past");
+        }
+    })
+};
+
+// Updates time block colors each minute 
+Colors();
+
+function Colors() {
+    var currentDateSeconds = new Date().getSeconds();
+    if (currentDateSeconds == 0) {
+        setInterval(currentDate, 60000);
+        setInterval(blockColors, 60000);
+    }
+    blockColors();
+};
+
+Colors();
+
+//Event Listener for Save Buttons
+saveBtn.on('click', saveButton);
